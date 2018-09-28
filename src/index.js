933232eaf53454363d827d3cdb6c8bbd36678ac6
halaveika <halaveika1988@gmail.com>
2018-09-29 02:59:13 +0300

    task8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let result = Number.MAX_VALUE;
+  let j = base;
+  for (let i = 2; i <= base; i++) {
+    if (j % i === 0) {
+      let p = 0;
+      while (j % i === 0) {
+         j = j / i;     
+         p++;
+      }
+      let c = 0;
+      let z = Math.floor(number / i);
+      while (z > 0) {
+        c += z;
+        z = Math.floor(z / i);
+      }
+      result = Math.min(result, Math.floor(c / p))
+    }
+  }
+  return module.exports=result;
+  
 }
